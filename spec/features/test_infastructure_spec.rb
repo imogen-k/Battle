136dd19500066f2c3c91dad4bd55fbdd9286d28a
imogenkraak <imogen.kraak@gmail.com>
2020-08-17 18:22:10 +0100

    added gems and first feature test

---
+++ spec/features/test_infastructure_spec.rb
@@ -0,0 +1,9 @@
+
+
+
+feature 'test for infrastructure' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
